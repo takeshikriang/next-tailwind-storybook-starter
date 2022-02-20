c8c08898bcb10b47c9d3266dcddec79b44de5fb2
tk <contact@kriang.com>
2022-02-20 17:21:44 +0700

    Refactor test to import component as Story

--- components/ui/__test__/Button.test.tsx
+++ components/ui/__test__/Button.test.tsx
@@ -1,10 +1,24 @@
+import { composeStories } from '@storybook/testing-react'
 import { render, screen } from '@testing-library/react'
 import { Button } from 'components/ui'
+import * as stories from 'components/ui/__stories__/Button.stories'
 
+const { Component } = composeStories(stories)
+
+// Import Button directly
 test('Button renders correctly', () => {
   render(<Button type="submit">Click me</Button>)
   const button = screen.queryByRole('button')
 
   expect(button).toHaveTextContent('Click me')
+  expect(button).toHaveAttribute('type', 'submit')
+})
+
+// Import Button as Story
+test('Button Story renders correctly', () => {
+  render(<Component type="submit" />)
+  const button = screen.queryByRole('button')
+
+  expect(button).toHaveTextContent('My Button')
   expect(button).toHaveAttribute('type', 'submit')
 })
